722d1871a9ee44c45b4036cbf75cbce0004c55f2
Vitor Carvalho <vdecarv@gmail.com>
2019-02-23 09:10:10 -0300

    working js

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,19 @@
 var wrapper = document.getElementById('pedals'),
     pedals = wrapper.getElementsByTagName('div');
 
-for (i=0; i < pedals.length; i++) {
+for (i = 0; i < pedals.length; i++) {
+    var pedal = pedals[i], audio;
+    
+    pedal.onmouseenter = function() {
+        audio = this.getElementsByTagName("audio")[0];
 
-    pedals[i].onclick = function() {
-        this.getElementsByTagName("audio")[0].play();
+        if (audio) audio.play();
+    }
+
+    pedals[i].onmouseleave = function() {
+        if (audio) {
+            audio.pause();
+            audio.currentTime = 0;
+        }
     }
 }
